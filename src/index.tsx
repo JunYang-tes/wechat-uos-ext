a9ccb93ab7f988def39b23a9f3c5ced7a8d996e1
YJ <jun.yang@target-energysolutions.com>
2023-05-17 17:39:38 +0800

    init

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+/* @refresh reload */
+import {init} from './inject/hook-points/proxy'
+import './inject/hook-points/setup'
+import './inject/hook-points/ipcRender'
+import './inject/screenshot'
+import './inject/prevent-recall'
+init()
