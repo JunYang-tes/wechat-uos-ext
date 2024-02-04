c26233f4b8fa7b63487252d2420f59403ccba58e
YJ <jun.yang@target-energysolutions.com>
2024-02-04 17:06:38 +0800

    paste ux

--- src/inject/paste-img.tsx
+++ src/inject/paste-img.tsx
@@ -5,34 +5,26 @@ import { emit } from "./hook-points/ipcRender";
 import { css } from '@emotion/css'
 import { primaryTxtButton, secondaryTxtButton } from "../utils/style";
 
-addDomAdditionHandler(
+function handlePasteImg(e: ClipboardEvent) {
+  if (e.clipboardData && e.clipboardData.files.length) {
+    const file = e.clipboardData.files[0]
+    if (file.type.startsWith('image')) {
 
-  (node) => node.nodeType === Node.ELEMENT_NODE && (node as HTMLElement).querySelector('#textContent') !== null,
-  (node) => {
-    const textContent = (node as HTMLElement).querySelector('#textContent') as HTMLElement
-    if (textContent) {
-      document.addEventListener(
-        'paste',
-        (e) => {
-          if (e.clipboardData && e.clipboardData.files.length) {
-            const file = e.clipboardData.files[0]
-            if (file.type.startsWith('image')) {
-
-              e.stopPropagation()
-              e.preventDefault()
+      e.stopPropagation()
+      e.preventDefault()
 
 
-              const reader = new FileReader()
-              reader.addEventListener('load', async (e) => {
-                const data: ArrayBuffer = reader.result as any
-                const unlink = require('fs/promises').unlink
-                try {
-                  await unlink('/tmp/wechat-img')
-                } catch {}
-                const write = require('fs/promises').writeFile
-                await write('/tmp/wechat-img', new Uint8Array(data))
-                let keydownHandler: (e: KeyboardEvent) => void
-                const unmount = render(() => <div class={css`
+      const reader = new FileReader()
+      reader.addEventListener('load', async (e) => {
+        const data: ArrayBuffer = reader.result as any
+        const unlink = require('fs/promises').unlink
+        try {
+          await unlink('/tmp/wechat-img')
+        } catch { }
+        const write = require('fs/promises').writeFile
+        await write('/tmp/wechat-img', new Uint8Array(data))
+        let keydownHandler: (e: KeyboardEvent) => void
+        const unmount = render(() => <div class={css`
                     position: absolute;
                     background-color: white;
                     top: 50px;
@@ -44,68 +36,89 @@ addDomAdditionHandler(
                     border-radius: var(--dialog-radius);
                     align-items: center;
                 `}>
-                  <img src="/tmp/wechat-img" class={css`max-width: 500vw;max-height:50vh;`} />
-                  <div class={css`
+          <img src="/tmp/wechat-img" class={css`max-width: 500vw;max-height:50vh;`} />
+          <div class={css`
                   display: flex;
     gap: 10px;
     padding-top: 10px;
     align-self: end;
                   `}>
-                    <button
-                      class={secondaryTxtButton}
-                      onClick={() => {
-                        document.removeEventListener('keydown', keydownHandler, { capture: true })
-                        unmount()
-                      }}>
-                      Cancel
-                    </button>
-                    <button
-                      class={primaryTxtButton}
-                      onClick={() => {
-                        document.removeEventListener('keydown', keydownHandler, { capture: true })
-                        emit('SCREENSHOT::SAVE_Local_Success',
-                          null, {
-                          filePath: '/tmp/wechat-img',
-                          isShare: false
-                        })
-                        unmount()
-                      }}>
-                      Send
-                    </button>
-                  </div>
-                </div>,
-                  getOrCreateMountPoint('#--paste-img'))
-
-                keydownHandler = (e: KeyboardEvent) => {
-                  console.log(e.key)
-                  if (e.key === 'Escape') {
-                    unmount()
-                    e.stopPropagation()
-                    e.preventDefault()
-                    document.removeEventListener('keydown', keydownHandler, { capture: true })
-                  }
-                  if (e.key === 'Enter') {
-                    unmount()
-                    emit('SCREENSHOT::SAVE_Local_Success',
-                      null, {
-                      filePath: '/tmp/wechat-img',
-                      isShare: false
-                    })
-                    e.stopPropagation()
-                    e.preventDefault()
-                    document.removeEventListener('keydown', keydownHandler, { capture: true })
-                  }
-
-                }
-                document.addEventListener('keydown', keydownHandler, {
-                  capture: true,
+            <button
+              class={secondaryTxtButton}
+              onClick={() => {
+                document.removeEventListener('keydown', keydownHandler, { capture: true })
+                unmount()
+              }}>
+              Cancel
+            </button>
+            <button
+              class={primaryTxtButton}
+              onClick={() => {
+                document.removeEventListener('keydown', keydownHandler, { capture: true })
+                emit('SCREENSHOT::SAVE_Local_Success',
+                  null, {
+                  filePath: '/tmp/wechat-img',
+                  isShare: false
                 })
-              })
-              reader.readAsArrayBuffer(
-                file
-              )
-            }
+                unmount()
+              }}>
+              Send
+            </button>
+          </div>
+        </div>,
+          getOrCreateMountPoint('#--paste-img'))
 
+        keydownHandler = (e: KeyboardEvent) => {
+          console.log(e.key)
+          if (e.key === 'Escape') {
+            unmount()
+            e.stopPropagation()
+            e.preventDefault()
+            document.removeEventListener('keydown', keydownHandler, { capture: true })
+          }
+          if (e.key === 'Enter') {
+            unmount()
+            emit('SCREENSHOT::SAVE_Local_Success',
+              null, {
+              filePath: '/tmp/wechat-img',
+              isShare: false
+            })
+            e.stopPropagation()
+            e.preventDefault()
+            document.removeEventListener('keydown', keydownHandler, { capture: true })
+          }
+
+        }
+        document.addEventListener('keydown', keydownHandler, {
+          capture: true,
+        })
+      })
+      reader.readAsArrayBuffer(
+        file
+      )
+    }
+
+  }
+}
+
+
+addDomAdditionHandler(
+  (node) => node.nodeType === Node.ELEMENT_NODE && (node as HTMLElement).querySelector('#textContent') !== null,
+  (node) => {
+    const textContent = (node as HTMLElement).querySelector('#textContent') as HTMLElement
+    if (textContent) {
+
+      document.addEventListener(
+        'paste',
+        (e) => {
+          let target = e.target as HTMLElement | null
+          if (target?.id === 'textContent' && e.clipboardData) {
+            if (e.clipboardData.types[0] === 'Files') {
+              handlePasteImg(e)
+            } else if (e.clipboardData.types[0].startsWith('text')) {
+              // tx 处理的onPaste 太粗糙，不如浏览自己的
+              e.stopPropagation();
+            }
           }
         },
         {
